12d29f801877c0206252976e2d514e828935be75
SyalomGabrielaWagey <90564531+SyalomGabrielaWagey@users.noreply.github.com>
2022-11-30 15:50:59 +0800

    Add file About.js

---
+++ src/components/About.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default function About () {
+    return (
+        <section classname="page-section" id="about">
+            <div classname="container">
+            <div classname="text-center">
+                <h2 classname="section-heading text-uppercase">About</h2>
+                <h3 classname="section-subheading text-muted">Phase Of TokyoService-Comunity</h3>
+            </div>
+            <ul classname="timeline">
+            <li>
+            <div classname="timeline-panel">
+            <div classname="timeline-heading">
+                <h4>Agustus 2022</h4>
+                <h4 classname="subheading">Design</h4>
+            </div>
+            <div classname="timeline-body"><p classname="text-muted">Dari awal berdirinya TokyoServices-Comunity, kami telah melakukan berbagai jenis usaha dalam menjadikan TokyoCleaning-Services perusahaan yang terbaik bagi anda. Salah satunya adalah dengan memberikan design website yang menarik mata dan perhatian anda.</p></div>
+            </div>
+            </li>
+            <li classname="timeline-inverted">
+            <div classname="timeline-panel">
+            <div classname="timeline-heading">
+                <h4>September 2022</h4>
+                <h4 classname="subheading">Born of TokyoService-Community</h4>
+            </div>
+            <div classname="timeline-body"><p classname="text-muted">Pada awal September 2022, kami telah berhasil mendirikan TokyoService-Community. Yang kantor utamanya, berlokasikan di Universitas Klabat, Airmadidi Bawah.</p></div>
+            </div>
+            </li>
+            <li>
+            <div classname="timeline-panel">
+            <div classname="timeline-heading">
+                <h4>Oktober 2022</h4>
+                <h4 classname="subheading">Services</h4>
+            </div>
+            <div classname="timeline-body"><p classname="text-muted">Kami melihat masalah yang sering dialami saat ini adalah masalah kebersihan rumah dan lingkungan. Terlebih lagi bagi mahasiswa dan para pekerja yang sibuk, sehingga kekurangan waktu untuk membersihkan rumah.</p></div>
+            </div>
+            </li>
+            <li classname="timeline-inverted">
+                <div classname="timeline-panel">
+                <div classname="timeline-heading">
+                    <h4>Desember 2022</h4>
+                    <h4 classname="subheading">TokyoService-Comunity in 2022</h4>
+                </div>
+                <div classname="timeline-body"><p classname="text-muted">Saat ini kami telah memiliki beberapa cabang yang sudah tersebar di kota-kota besar seperti Manado, Bitung, Tomohon dan juga cabang diluar pulai yang berada di Jakarta Selatan.</p></div>
+                </div>
+            </li>
+            </ul>
+        </div>
+    </section>
+    )
+}
